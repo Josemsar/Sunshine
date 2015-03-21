9348360e678859050f3ee83f8c3b68f5c868ece3
Jose Miguel Sarasola <Josemsar92@gmail.com>
2015-03-21 23:25:15 +0100

    Lesson 3 - Units & Location added.

--- app/src/main/java/com/josemsar/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/josemsar/sunshine/app/DetailActivity.java
@@ -2,29 +2,31 @@ package com.josemsar.sunshine.app;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.*;
+import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class DetailActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_detail);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new ForecastFragment())
+                    .add(R.id.container, new DetailFragment())
                     .commit();
         }
+
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_detail, menu);
         return true;
     }
 
@@ -42,5 +44,24 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    public static class DetailFragment extends Fragment {
+
+        public DetailFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+
+            Intent intent = getActivity().getIntent();
+            ((TextView) rootView.findViewById(R.id.detail_text))
+                    .setText(intent.getExtras().getString(Intent.EXTRA_TEXT));
+
+            return rootView;
+        }
     }
 }
